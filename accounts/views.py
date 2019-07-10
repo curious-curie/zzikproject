e3321aad68c8a3b02df44cab2fbe9be42ce8518d
curious-curie <curious.curieyoo@gmail.com>
2019-07-10 21:17:13 +0900

    프론트 로그인 로그아웃 바뀌게

--- accounts/views.py
+++ accounts/views.py
@@ -25,4 +25,4 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    return redirect('/reviews')
