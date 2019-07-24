a9c0110b14250071c39c74b331b7f7279b66b973
psyeon990 <psyeon990@naver.com>
2019-07-24 16:11:49 +0900

    def detail

--- zzikplace/views.py
+++ zzikplace/views.py
@@ -12,6 +12,20 @@ def around(request):
 def new(request):
     return render(request, 'zzikplace/new.html')
 
-def detail(request, id):
-    place = Place.objects.get(id=id)
-    return render(request, 'zzikplace/detail.html', {'place': place})
+def detail(request, id=''):
+    if request.method == "POST":
+        title = request.POST['place_name']
+        address = reqest.POST['place_addr']
+        tip = request.POST['tip']
+        photo = request.POST['photo']
+        time = request.POST['time']
+        place, is_place = Place.objects.get_or_create(address=address, title=title)
+        id = place.id
+        Review.objects.create(place_id=id, tip=tip, photo=photo, time=time, author=request.user)
+        return render(request, 'zzikplace/detail.html', {'place': place})
+
+    elif request.method == "GET":
+        place = Place.objects.get(id=id)
+        return render(request, 'zzikplace/detail.html', {'place': place})
+
+        
