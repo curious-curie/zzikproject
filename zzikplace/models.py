83323c258536a2dfc7c47a16cd6ee3b8dfbae88b
curious-curie <curious.curieyoo@gmail.com>
2019-08-05 08:49:43 +0900

    work in process : tags

--- zzikplace/models.py
+++ zzikplace/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save  # 추가
 from django.dispatch import receiver  
 from django.utils import timezone
+import re
 
 # class Profile(models.Model):   # 추가
 #     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -17,7 +18,30 @@ class Place(models.Model):
     x = models.FloatField(default = None)
     y = models.FloatField(default = None)
     saved_users = models.ManyToManyField(User, blank=True, related_name='places_saved', through='Save')
+    tag_content = models.TextField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ['-created_at']
+    
+    def tag_save(self):
+        tags = re.findall(r'#(\w+)\b', self.tag_content)
+
+        if not tags:
+            return
+        
+        for t in tags:
+            tag, tag_created = Tag.objects.get_or_create(name=t)
+            self.tag_set.add(tag)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=140, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Review(models.Model):
     TIME_CHOICES = (
         ('A', '해 뜰 무렵'),
@@ -35,7 +59,7 @@ class Review(models.Model):
     time = models.CharField(max_length=1, choices=TIME_CHOICES)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(blank=True, null=True)
-
+    
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -46,6 +70,7 @@ class Save(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 # @receiver(post_save, sender=User)
 # def create_user_profile(sender, instance, created, **kwargs):  
