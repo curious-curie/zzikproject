73ac63b6165a44b5ee8e8da3b124c331b7283d8c
Snowman <snowman@gimmingiui-MacBook-Pro.local>
2019-08-18 03:51:54 +0900

    로그인 회원가입 안내문 구현함

--- accounts/views.py
+++ accounts/views.py
@@ -7,18 +7,22 @@ from django import forms
 def signup(request):
     if request.method  == 'POST':
         if len(request.POST['password1']) < 6:
-            raise forms.ValidationError("Your password should be at least 6 Characters")
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
-            auth.login(request, user)
-            profile = user.profile
-            profile.nickname = request.POST['nickname']
-            user.save()
-            return redirect('/reviews')
-        else:
-            messages.error(request,'입력하신 비밀번호가 같은지 다시 한번 확인해주세요')
-            #return render_to_response('template_name')
+            messages.error(request,'입력하신 비밀번호가 너무 짧습니다.(최소 6글자 이상)')
             return render(request, 'accounts/signup.html')
+            #raise forms.ValidationError("Your password should be at least 6 Characters")
+        
+        else:
+            if request.POST['password1'] == request.POST['password2']:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                profile = user.profile
+                profile.nickname = request.POST['nickname']
+                user.save()
+                return redirect('/reviews')
+            else:
+                messages.error(request,'입력하신 비밀번호가 같은지 다시 한번 확인해주세요')
+                #return render_to_response('template_name')
+                return render(request, 'accounts/signup.html')
 
 
     return render(request, 'accounts/signup.html')
